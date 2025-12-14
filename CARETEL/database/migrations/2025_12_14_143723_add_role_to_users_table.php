7bf09748eb65e62c1121cf613485ae4336e526c7
AraZahra26 <zahra.daniah06@gmail.com>
2025-12-15 01:12:22 +0700

    PERBAIKAN1

--- CARETEL/database/migrations/2025_12_14_143723_add_role_to_users_table.php
+++ CARETEL/database/migrations/2025_12_14_143723_add_role_to_users_table.php
@@ -11,22 +11,26 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-        $table->enum('role', ['admin', 'mahasiswa', 'teknisi'])->default('mahasiswa');
-        $table->string('phone')->nullable();
-        $table->string('photo')->nullable();
-        $table->boolean('is_active')->default(true);
-    });
+        if (Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->enum('role', ['admin', 'mahasiswa', 'teknisi'])->default('mahasiswa');
+                $table->string('phone')->nullable();
+                $table->string('photo')->nullable();
+                $table->boolean('is_active')->default(true);
+            });
+        }
 
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+        public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        if (Schema::hasTable('users')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn(['role', 'phone', 'photo', 'is_active']);
+            });
+        }
     }
 };
